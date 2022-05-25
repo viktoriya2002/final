a5e013c8232ca38e0929511169a75645abf50e7d
077-19 <pivovarova-2002@bk.ru>
2022-05-25 18:39:12 +0300

    v4

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,62 +1,72 @@
 import React, {useState} from 'react';
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 const Auth = () => {
-    let [user, setUser]=useState({email:'',password:''})
-    function send(e){
+    let history = useNavigate()
+    async function my_request(e) {
         e.preventDefault()
-        let body=JSON.stringify(user)
-        console.log(user)
-        let myHeaders = new Headers();
-        myHeaders.append("Authorization", `Bearer ${localStorage.token}`)
-        myHeaders.append("Content-Type", "application/json")
-        let request_options={headers: myHeaders, body: body, method: 'POST'}
-        fetch('http://pets.сделай.site/api/login', request_options)
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
 
-            .then(response=>{let status=response.status
-                switch(status){
-                    case 204: document.getElementById('message').style.display='block'
-                        document.getElementById('error').style.display='none'
-                        break
-                    case 422: document.getElementById('message').style.display='none'
-                        document.getElementById('error').style.display='block'
-                        break
-                }}
-            )}
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: JSON.stringify(Login)
+
+        };
+        console.log(requestOptions)
+        let res = fetch("http://pets.сделай.site/api/login", requestOptions)
+            .then(response=>response.json())
+
+            //.then(response=>localStorage.token=response.data.token)
+            .then(response=>{console.log(response)
+                if ('data' in response){
+                    localStorage.token=response.data.token
+                    history("/profile")
+                } else document.getElementById('ERROR').style.display='block'
+            })
+    }
+
+    let [Login, setLogin] = useState({email:'',password:''});
+    localStorage.token=null
     return (
         <div>
-            <main style={{"minHeight": "70vh"}}>
+
+            <div style={{"minHeight": "70vh"}}>
                 <h2 className="text-center text-white bg-success m-2">Вход в кабинет</h2>
+                <div className="divider-custom">
+                </div>
                 <div className="p-3">
-                    <form onSubmit={send} className="w-50 m-auto border p-3" style={{"minWidth": "300px"}}>
+                    <form className="w-50 m-auto border p-3" style={{"minWidth": "300px"}} onSubmit={my_request}>
 
                         <div className="mb-3">
                             <label htmlFor="email" className="form-label">Введите адрес электронной почты</label>
-                            <input type="name" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e)=>setUser({user, email: e.target.value})}/>
-                            <div className="form-text">Мы никогда не делимся Вашими e-mail ни с кем.</div>
+                            <input type="email" className="form-control" id="email" required onChange={(e)=>setLogin({...Login, email:e.target.value})}/>
+                            <div className="form-text">Мы никогда не делимся Вашим e-mail</div>
                         </div>
 
                         <div className="mb-3">
                             <label htmlFor="password" className="form-label">Введите пароль:</label>
-                            <input type="password" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e)=>setUser({user, password: e.target.value})}/>
+                            <input type="password" className="form-control" id="password" required pattern={'/^(?=.*[A-Z])(?=.*[\\d])(?=.*[a-z]){7,}$/'} onChange={(e)=>setLogin({...Login, password:e.target.value})}/>
                             <div className="form-text">Не менее 7 символов, обязательно: 1 - цифра, 1 - строчная
                                 буква, 1 - заглавная буква
                             </div>
                         </div>
 
-                        <div className="row_align">
-                            <Link to ={'/profile'}>
-                                <input type="submit" className="btn btn-success"/>
-                            </Link>
-                        </div>
+                        <input type={"submit"} className="btn btn-primary" value={'Войти'}/>
                     </form>
                 </div>
-            </main>
-
-            <div className={'text-center border-1 border-info rounded-1 border p-1 m-3'} id={'message'} style={{display: 'none'}}>Вход выполнен!</div>
-            <div className={'text-center border-1 border-danger rounded-1 border p-1 m-3'} id={'error'} style={{display: 'none'}}>Вход не выполнен.</div>
-
+            </div>
+            <div className="p-3" id={'ERROR'} style={{display:'none'}}>
+                <form className="w-50 m-auto border p-3" style={{"minWidth": "300px"}}>
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Ошибка входа в кабинет. Перероверьте почту или пароль.</label>
+                    </div>
+                </form>
+            </div>
         </div>
+
     );
 };
 
